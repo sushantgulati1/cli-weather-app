d7d9f1db8f8f9d200ba16b63df6c9e713351e464
Sushant Gulati <sushant030898@gmail.com>
2018-07-25 17:18:14 +0530

    Added error handling and improvements

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ app.set('view engine', 'pug')
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var api_key = '37655193ca4ccb434c6ccb6c7b378e80';
-var message='';
+var api_key = '**********';
 
 app.get('/', function (req, res) {
   res.render('index');
 })
 
 app.post('/', function (req, res) {
-    
+    var message='';
     var cityname = req.body.city;
     var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=imperial&appid=${api_key}`;
     request(url, function (err, response, body) {
@@ -25,19 +24,25 @@ app.post('/', function (req, res) {
     }
     else {
         let result = JSON.parse(body)
-	message+= `Temperature in ${result.name} is `+ convertToCelcius(`${result.main.temp}`) +`°C, ${result.weather[0].main}`;
-        message+= `\nMax Temp: `+ convertToCelcius(`${result.main.temp_max}`) +`°C`;
-        message+= `\nMin Temp: `+ convertToCelcius(`${result.main.temp_min}`) +`°C`;
-        console.log("Msg sent: " + message);
-	res.render('index', {msg:message});
-	
+        //Invalid city value doesn't return main property in JSON
+    	if(result.main == undefined){
+            message += "Invalid City or API key, Please Try Again";
+        }
+    	else {
+    	    message+= `Temperature in ${result.name} is `+ convertToCelcius(`${result.main.temp}`) +`°C, ${result.weather[0].main}`;
+            message+= `\nMax Temp: `+ convertToCelcius(`${result.main.temp_max}`) +`°C `;
+            message+= `\nMin Temp: `+ convertToCelcius(`${result.main.temp_min}`) +`°C`;
+            console.log("Msg sent: " + message);
+    	}
+    	res.render('index', {msg:message});
+
     }
 })
 });
 
 function convertToCelcius(temp){
     temp = (temp-32)*5/9;
-    temp = Math.round(temp*100)/100;
+    temp = Math.round(temp*10)/10;
     return temp;
 }
 
